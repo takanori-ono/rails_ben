e406d167f04b005885c7f595f87e0c2fd203c838
Ono, Takanori <takanori3ono@gmail.com>
2023-08-06 19:06:23 +0900

    掲示板の作成完了

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'login', to: "sessions#new"
   post 'login', to: "sessions#create"
   get 'logout', to: "sessions#destroy"
+  resources :posts, only: [:index, :new, :create, :edit, :update, :destroy]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_03_232326) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_06_014258) do
   create_table "notes", force: :cascade do |t|
     t.string "subject"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
